875ccbad73ee304340f86160cd72961fdae99da7
John Elarcosa Davila <davilajohn810@gmail.com>
2023-09-23 00:26:32 +0800

    Update settings.py

--- backend/concert_ticket/settings.py
+++ backend/concert_ticket/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "testwerbwerwerwe"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG  = True
+DEBUG  = False
 
 ALLOWED_HOSTS = ["*"]
 
